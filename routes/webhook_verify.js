1f171d75739bd8a26ebad092129e610204c19700
Tzezanas <orenlibane@gmail.com>
2021-01-22 08:17:36 +0200

    adding the webhook

--- routes/webhook_verify.js
+++ routes/webhook_verify.js
@@ -1,5 +1,5 @@
 const processPostback = require('../processes/postback');
-const processMessage = require('../processes/messages');
+// const processMessage = require('../processes/messages');
 
 module.exports = function(app, chalk){
     app.get('/webhook', function(req, res) {
