43b14d7bd81c66639228a26a0635331f184d20cc
Tzezanas <orenlibane@gmail.com>
2021-01-29 18:09:50 +0200

    me and danielle playing with the bot messages!

--- processes/messages.js
+++ processes/messages.js
@@ -7,6 +7,21 @@ module.exports = function processMessage(event) {
         const senderID = event.sender.id;
         if (message.text) {
             senderAction(senderID);
+            sendMessage(senderID, {text: 'type of answer' + typeof message.text})
+            sendMessage(senderID, {text: navigator.userAgent})
+            switch (message.text) {
+                case '1':
+                    // send to tel
+                    // check if mobile device and give currect answer.
+                    window.open('tel:2344')
+                    break;
+                case '2':
+                    // show menu/send to menu Link
+                    break;
+                case '3':
+                    // make online Delivery - send Wolt?
+                    break;
+            }
             const messageTest = `BLA BLA RESPONSE! + \n ${message.text}`
             sendMessage(senderID, {text: messageTest})
         }
